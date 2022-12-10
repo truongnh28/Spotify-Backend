2b139a068a927261aae487cfa8d9f1c4a0d514e0
truongnh3 <truongnh3@vng.com.vn>
2022-12-10 19:38:10 +0700

    refactor models

--- models/models.go
+++ models/models.go
@@ -7,74 +7,74 @@ import (
 
 type Albums struct {
 	*gorm.Model
-	AlbumID   uint      `gorm:"column:album_ID"`
-	Name      string    `gorm:"column:name"`
-	ArtistID  uint      `gorm:"column:artist_ID"`
-	Artists   Artists   `gorm:"foreignKey:ArtistID;references:ID"`
-	CreateAt  time.Time `gorm:"column:create_at"`
-	UploadAt  time.Time `gorm:"column:upload_at"`
-	DeletedAt time.Time `gorm:"column:deleted_at"`
-	CoverImg  string    `gorm:"column:cover_img"`
+	AlbumID    uint      `gorm:"column:id"`
+	Name       string    `gorm:"column:name"`
+	ArtistID   uint      `gorm:"column:artist_ID"`
+	Artists    Artists   `gorm:"foreignKey:id;references:id"`
+	CreatedAt  time.Time `gorm:"column:created_at"`
+	UploadedAt time.Time `gorm:"column:uploaded_at"`
+	DeletedAt  time.Time `gorm:"column:deleted_at"`
+	CoverImg   string    `gorm:"column:cover_img"`
 }
 
 type Artists struct {
 	*gorm.Model
-	ArtistID    uint      `gorm:"column:artist_ID"`
+	ArtistID    uint      `gorm:"column:id"`
 	Name        string    `gorm:"column:name"`
 	Description string    `gorm:"column:description"`
-	CreateAt    time.Time `gorm:"column:create_at"`
-	UploadAt    time.Time `gorm:"column:upload_at"`
+	CreatedAt   time.Time `gorm:"column:created_at"`
+	UploadedAt  time.Time `gorm:"column:uploaded_at"`
 	DeletedAt   time.Time `gorm:"column:deleted_at"`
 	CoverImg    string    `gorm:"column:cover_img"`
 }
 
 type Interactions struct {
 	*gorm.Model
-	InteractionID uint      `gorm:"column:interaction_ID"`
+	InteractionID uint      `gorm:"column:id"`
 	UserID        uint      `gorm:"column:user_ID"`
-	Users         Accounts  `gorm:"foreignKey:Id;references:ID"`
+	Users         Accounts  `gorm:"foreignKey:id;references:id"`
 	SongID        uint      `gorm:"column:song_ID"`
-	Songs         Songs     `gorm:"foreignKey:SongID;references:ID"`
+	Songs         Songs     `gorm:"foreignKey:song_ID;references:id"`
 	Liked         uint      `gorm:"column:liked"`
 	PlayDuration  time.Time `gorm:"column:play_duration"`
-	CreateAt      time.Time `gorm:"column:create_at"`
-	UploadAt      time.Time `gorm:"column:upload_at"`
+	CreatedAt     time.Time `gorm:"column:created_at"`
+	UploadedAt    time.Time `gorm:"column:uploaded_at"`
 	DeletedAt     time.Time `gorm:"column:deleted_at"`
 }
 
 type PlayLists struct {
 	*gorm.Model
-	Id        uint      `gorm:"column:id"`
-	Name      string    `gorm:"column:name"`
-	CreateAt  time.Time `gorm:"column:create_at"`
-	UploadAt  time.Time `gorm:"column:upload_at"`
-	DeletedAt time.Time `gorm:"column:deleted_at"`
-	CoverImg  string    `gorm:"column:cover_img"`
-	UserID    uint      `gorm:"column:user_ID"`
-	Users     Accounts  `gorm:"foreignKey:Id;references:ID"`
+	PlayListID uint      `gorm:"column:id"`
+	Name       string    `gorm:"column:name"`
+	CreatedAt  time.Time `gorm:"column:created_at"`
+	UploadedAt time.Time `gorm:"column:uploaded_at"`
+	DeletedAt  time.Time `gorm:"column:deleted_at"`
+	CoverImg   string    `gorm:"column:cover_img"`
+	UserID     uint      `gorm:"column:user_ID"`
+	Users      Accounts  `gorm:"foreignKey:Id;references:id"`
 }
 
 type PlayListSongs struct {
 	*gorm.Model
 	SongID     uint      `gorm:"column:song_ID"`
-	Songs      Songs     `gorm:"foreignKey:SongID;references:ID"`
+	Songs      Songs     `gorm:"foreignKey:SongID;references:id"`
 	PlayListID uint      `gorm:"column:playlist_ID"`
-	PlayLists  PlayLists `gorm:"foreignKey:Id;references:ID"`
+	PlayLists  PlayLists `gorm:"foreignKey:Id;references:id"`
 }
 
 type Songs struct {
 	*gorm.Model
-	SongID      uint      `gorm:"column:song_ID"`
+	SongID      uint      `gorm:"column:id"`
 	Name        string    `gorm:"column:name"`
 	AlbumID     uint      `gorm:"column:album_ID"`
-	Albums      Albums    `gorm:"foreignKey:AlbumID;references:ID"`
+	Albums      Albums    `gorm:"foreignKey:AlbumID;references:id"`
 	ArtistID    uint      `gorm:"column:artist_ID"`
-	Artists     Artists   `gorm:"foreignKey:ArtistID;references:ID"`
+	Artists     Artists   `gorm:"foreignKey:ArtistID;references:id"`
 	Lyrics      string    `gorm:"column:lyrics"`
 	Length      uint      `gorm:"column:length"`
 	TrackNumber uint      `gorm:"column:track_number"`
-	CreateAt    time.Time `gorm:"column:create_at"`
-	UploadAt    time.Time `gorm:"column:upload_at"`
+	CreatedAt   time.Time `gorm:"column:created_at"`
+	UploadedAt  time.Time `gorm:"column:uploaded_at"`
 	DeletedAt   time.Time `gorm:"column:deleted_at"`
 	YoutubeLink string    `gorm:"column:youtube_link"`
 }
@@ -124,16 +124,6 @@ func (a AccountRole) IsValid() bool {
 	_, ok := priority[a]
 	return ok
 }
-
-// Status enum
-type Status byte
-
-const (
-	InitialStatus = iota
-	WaitingForScanStatus
-	ScanningStatus
-	ScannedStatus
-)
 
 type Table interface {
 	TableName() string
