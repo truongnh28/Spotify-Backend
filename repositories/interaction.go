f0984fa80efb06b2f247a7472f8b9677c482041f
truongnh3 <truongnh3@vng.com.vn>
2022-12-23 01:50:41 +0700

    fix add/remove interaction

--- repositories/interaction.go
+++ repositories/interaction.go
@@ -25,31 +25,65 @@ func NewInteractionRepository(database *gorm.DB) InteractionRepository {
 func (i *interactionRepositoryImpl) AddInteraction(ctx context.Context, userId, songId uint) (models.Interaction, error) {
 	var (
 		db    = i.database.WithContext(ctx)
+		inter = models.Interaction{}
+	)
+	db.Begin()
+	query := db.Model(&models.Interaction{}).Where("user_id = ? AND song_id = ?", userId, songId).First(&inter)
+	err := query.Error
+	if err != nil {
+		if err.Error() != "record not found" {
+			db.Rollback()
+			return inter, err
+		}
+	}
+	if err != nil {
 		inter = models.Interaction{
 			UserID: userId,
 			SongID: songId,
 			Liked:  1,
 		}
-	)
-	err := db.Model(&models.Interaction{}).Where("user_id = ? AND song_id = ?", userId, songId).FirstOrCreate(&inter).Error
-	if err != nil {
-		return models.Interaction{}, err
+		err = db.Model(&models.Interaction{}).Create(&inter).Error
+	} else {
+		inter.Liked = 1
+		err = db.Model(&models.Interaction{}).Where("user_id = ? AND song_id = ?", userId, songId).Update("liked", 1).Error
 	}
-	return inter, err
+	if err != nil {
+		db.Rollback()
+		return inter, err
+	}
+	db.Commit()
+	return inter, nil
 }
 
 func (i *interactionRepositoryImpl) RemoveInteraction(ctx context.Context, userId, songId uint) (models.Interaction, error) {
 	var (
 		db    = i.database.WithContext(ctx)
+		inter = models.Interaction{}
+	)
+	db.Begin()
+	query := db.Model(&models.Interaction{}).Where("user_id = ? AND song_id = ?", userId, songId).First(&inter)
+	err := query.Error
+	if err != nil {
+		if err.Error() != "record not found" {
+			db.Rollback()
+			return inter, err
+		}
+	}
+	if err != nil {
 		inter = models.Interaction{
 			UserID: userId,
 			SongID: songId,
 			Liked:  0,
 		}
-	)
-	err := db.Model(&models.Interaction{}).Where("user_id = ? AND song_id = ?", userId, songId).FirstOrCreate(&inter).Error
-	if err != nil {
-		return models.Interaction{}, err
+		err = db.Model(&models.Interaction{}).Create(&inter).Error
+	} else {
+		inter.Liked = 0
+		err = db.Model(&models.Interaction{}).Where("user_id = ? AND song_id = ?", userId, songId).Update("liked", 0).Error
 	}
-	return inter, err
+	if err != nil {
+		db.Rollback()
+		return inter, err
+	}
+	db.Commit()
+	return inter, nil
 }
